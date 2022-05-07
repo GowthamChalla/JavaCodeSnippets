dadaabe6ba07f3fae27fb6b954d1f60da9eed245
Gowthamchalla <Gowtham_Challa@outlook.com>
2022-05-07 18:43:01 -0400

    Arrayclass

---
+++ src/Collections/ArrayClassExample1_Class.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.Arrays;
+
+public class ArrayClassExample1_Class {
+
+	public static void main(String[] args) 
+	{
+		int[] arr = new int[] {10,20,25,19,30,40,50};
+		
+		String str = Arrays.toString(arr);
+		System.out.println(str);
+		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));		
+
+	}
+
+}
