4b6c714a90ad014682bf1b97d02efdd6faa08efa
Gowthamchalla <Gowtham_Challa@outlook.com>
2022-05-07 18:33:10 -0400

    Scanner Class and document

---
+++ src/Collections/ScannerClassExample1_Class.java
@@ -0,0 +1,16 @@
+package Collections;
+
+import java.util.Scanner;
+
+public class ScannerClassExample1_Class {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("Enter the Number ::");
+		int x = sc.nextInt();
+		System.out.println("Entered Number - "+x);
+
+	}
+
+}
